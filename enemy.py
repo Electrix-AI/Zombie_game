0d0c4c30b0048e01a0adc000b46c9cb51bce9391
PeculiarC <150746034+PeculiarC@users.noreply.github.com>
2025-04-02 13:24:46 -0500

    enemy.py
    added attack for zombie

--- enemy.py
+++ enemy.py
@@ -3,13 +3,13 @@ import random as r
 #have multiple enemies for now 
 class enemy():
     def __init__(self,baseA,baseH):
-        self._baseA = r.uniform(0.0,10.0) + baseA
-        self._baseH = r.uniform(0.0,10.0) + baseH
+        self._baseA = int(r.uniform(0.0,10.0) + baseA)
+        self._baseH = int(r.uniform(0.0,10.0) + baseH)
     def getatk(self):
         return self._baseA
     def gethp(self):
         return self._baseH
-    def atk():
+    def atk(self,playerH):
         pass
 class zombieE(enemy):
     def __init__(self, baseA, baseH):
@@ -18,9 +18,16 @@ class zombieE(enemy):
         return super().getatk()
     def gethp(self):
         return super().getatk()
-    def infection(self):
-        zIn = r.uniform(0.0,3.0)
+    def infection(self,cinfect):
+        zIn = r.uniform(0.0,3.0) + cinfect #total infection rate of player
+        #print("Player's infection is now at:")
         return zIn
+    def atk(self,playerH,cinfect):
+        playerH = playerH - self._baseA
+        if(r.random() > 0.5):
+            infection(cinfect) # type: ignore
+        #print("Player health now at:",playerH)
+        return playerH
     
 
 class banditE(enemy):
